e5ccbfb837c37739cc8132d31ed2dab9919f47e4
James Bowen <jhbowen047@gmail.com>
2018-02-03 15:50:08 -0800

    Use form URL Encoded

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser');
 
 var app = express()
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/api/hello', function(req, res) {
   res.send('Hello');
